8ee98d9e5c9e86119faf79c6049859bbcbc995af
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2016-12-22 10:02:37 -0800

    Creating the app scaffold

---
+++ expense_tracker/README.txt
@@ -0,0 +1,14 @@
+expense_tracker README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/pip install -e .
+
+- $VENV/bin/initialize_expense_tracker_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ expense_tracker/setup.py
@@ -0,0 +1,58 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'pyramid_ipython',
+    'ipython',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    'tox'
+    ]
+
+setup(name='expense_tracker',
+      version='0.0',
+      description='expense_tracker',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='Nicholas Hunt-Walker',
+      author_email='nhuntwalker@gmail.com',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = expense_tracker:main
+      [console_scripts]
+      initialize_db = expense_tracker.scripts.initializedb:main
+      """,
+      )
