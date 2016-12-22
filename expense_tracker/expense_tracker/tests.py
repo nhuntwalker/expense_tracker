69f81701b145671110a6725d6c03a66f6784a2cd
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2016-12-22 13:30:27 -0800

    Functional tests now working. Need to add more documentation

--- expense_tracker/expense_tracker/tests.py
+++ expense_tracker/expense_tracker/tests.py
@@ -3,7 +3,7 @@ import transaction
 
 from pyramid import testing
 
-from .models import Expense
+from .models import Expense, get_tm_session
 from .models.meta import Base
 
 import faker
@@ -22,8 +22,8 @@ def configuration():
 
 @pytest.fixture()
 def db_session(configuration, request):
-    Session_Factory = configuration.registry['dbsession_factory']
-    session = Session_Factory()
+    SessionFactory = configuration.registry['dbsession_factory']
+    session = SessionFactory()
     engine = session.bind
     Base.metadata.create_all(engine)
 
@@ -81,37 +81,49 @@ def test_list_view_returns_empty_when_empty(dummy_request, add_models):
     result = list_view(dummy_request)
     assert len(result["expenses"]) == 100
 
-# # ======== FUNCTIONAL TESTS ===========
+# ======== FUNCTIONAL TESTS ===========
 
 
-# @pytest.fixture
-# def testapp():
-#     from webtest import TestApp
-#     from expense_tracker import main
+@pytest.fixture
+def testapp():
+    from webtest import TestApp
+    from expense_tracker import main
 
-#     app = main({}, **{"sqlalchemy.url": 'sqlite:///:memory:'})
-#     testapp = TestApp(app)
-    
-#     Session_Factory = app.registry["dbsession_factory"]
-#     engine = Session_Factory().bind
-#     Base.metadata.create_all(bind=engine)
+    app = main({}, **{"sqlalchemy.url": 'sqlite:///:memory:'})
+    testapp = TestApp(app)
 
-#     return testapp
+    SessionFactory = app.registry["dbsession_factory"]
+    engine = SessionFactory().bind
+    Base.metadata.create_all(bind=engine)
 
-# @pytest.fixture
-# def fill_the_db(testapp):
-#     import pdb; pdb.set_trace()
-#     # with transaction.manager:
+    return testapp
 
 
-# def test_home_route_has_table(testapp):
-#     """The home page has a table."""
-#     response = testapp.get('/', status=200)
-#     html = response.html
-#     assert len(html.find_all("table")) == 1
+@pytest.fixture
+def fill_the_db(testapp):
+    SessionFactory = testapp.app.registry["dbsession_factory"]
+    with transaction.manager:
+        dbsession = get_tm_session(SessionFactory, transaction.manager)
+        dbsession.add_all(EXPENSES)
 
 
-# def test_home_route_with_data_has_filled_table(testapp, fill_the_db):
-#     """When there's data in the database, the home page has rows."""
-#     pass
+def test_home_route_has_table(testapp):
+    """The home page has a table."""
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(html.find_all("table")) == 1
+
+
+def test_home_route_with_data_has_filled_table(testapp, fill_the_db):
+    """When there's data in the database, the home page has rows."""
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(html.find_all("tr")) == 101
+
+
+def test_home_route_has_table2(testapp):
+    """The home page has a table with no rows."""
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(html.find_all("tr")) == 1
 
