c3d9d27bc7c23cff11d23f062a5af97188cfcc09
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2016-12-22 13:15:09 -0800

    The unit tests work. Now for the functional tests

--- expense_tracker/expense_tracker/tests.py
+++ expense_tracker/expense_tracker/tests.py
@@ -3,12 +3,7 @@ import transaction
 
 from pyramid import testing
 
-from .models import (
-    Expense,
-    get_engine,
-    get_session_factory,
-    get_tm_session,
-)
+from .models import Expense
 from .models.meta import Base
 
 import faker
@@ -16,40 +11,42 @@ import random
 import datetime
 
 
-# set up a SQL DB for the entire testing session.
-@pytest.fixture(scope="session")
-def sqlengine(request):
-    config = testing.setUp(settings={
-        'sqlalchemy.url': 'sqlite:///:memory:'
-    })
-    config.include(".models")
-    settings = config.get_settings()
-    engine = get_engine(settings)
+@pytest.yield_fixture(scope="session")
+def configuration():
+    settings = {'sqlalchemy.url': 'sqlite:///:memory:'}
+    config = testing.setUp(settings=settings)
+    config.include('.models')
+    yield config
+    testing.tearDown()
+
+
+@pytest.fixture()
+def db_session(configuration, request):
+    Session_Factory = configuration.registry['dbsession_factory']
+    session = Session_Factory()
+    engine = session.bind
     Base.metadata.create_all(engine)
 
     def teardown():
-        testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(engine)
-
-    request.addfinalizer(teardown)
-    return engine
-
-
-@pytest.fixture(scope="function")
-def new_session(sqlengine, request):
-    session_factory = get_session_factory(sqlengine)
-    session = get_tm_session(session_factory, transaction.manager)
-
-    def teardown():
-        transaction.abort()
+        session.transaction.rollback()
 
     request.addfinalizer(teardown)
     return session
 
+
+@pytest.fixture
+def dummy_request(db_session):
+    return testing.DummyRequest(dbsession=db_session)
+
+
+@pytest.fixture
+def add_models(dummy_request):
+    dummy_request.dbsession.add_all(EXPENSES)
+
 fake = faker.Faker()
 
 CATEGORIES = [
+
     "rent",
     "utilities",
     "groceries",
@@ -70,24 +67,21 @@ EXPENSES = [Expense(
 ) for i in range(100)]
 
 
-@pytest.fixture
-def http_request(new_session):
-    the_request = testing.DummyRequest()
-    the_request.dbsession = new_session
-    return the_request
+# ======== UNIT TESTS ==========
 
-
-def test_new_expenses_are_added(new_session):
+def test_new_expenses_are_added(db_session):
     """New expenses get added to the database."""
-
-    query = new_session.query(Expense).all()
+    db_session.add_all(EXPENSES)
+    query = db_session.query(Expense).all()
     assert len(query) == len(EXPENSES)
 
 
-def test_list_view_returns_empty_when_empty(http_request):
+def test_list_view_returns_empty_when_empty(dummy_request, add_models):
     from .views.default import list_view
-    result = list_view(http_request)
-    assert len(result["expenses"]) == 0
+    result = list_view(dummy_request)
+    assert len(result["expenses"]) == 100
+
+# # ======== FUNCTIONAL TESTS ===========
 
 
 # @pytest.fixture
@@ -96,7 +90,18 @@ def test_list_view_returns_empty_when_empty(http_request):
 #     from expense_tracker import main
 
 #     app = main({}, **{"sqlalchemy.url": 'sqlite:///:memory:'})
-#     return TestApp(app)
+#     testapp = TestApp(app)
+    
+#     Session_Factory = app.registry["dbsession_factory"]
+#     engine = Session_Factory().bind
+#     Base.metadata.create_all(bind=engine)
+
+#     return testapp
+
+# @pytest.fixture
+# def fill_the_db(testapp):
+#     import pdb; pdb.set_trace()
+#     # with transaction.manager:
 
 
 # def test_home_route_has_table(testapp):
@@ -104,3 +109,9 @@ def test_list_view_returns_empty_when_empty(http_request):
 #     response = testapp.get('/', status=200)
 #     html = response.html
 #     assert len(html.find_all("table")) == 1
+
+
+# def test_home_route_with_data_has_filled_table(testapp, fill_the_db):
+#     """When there's data in the database, the home page has rows."""
+#     pass
+
